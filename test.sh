b32d34a99fdcfd5f754752e1c2ca0a4ef7bcebf8
manish <manish.pyati@gmail.com>
2019-12-18 09:00:40 +0530

    test.sh updated

--- test.sh
+++ test.sh
@@ -4,10 +4,9 @@ read -p "Enter dir1 name:" dir1
 if [ -d "$dir1" ];
 then
         echo "$dir1 Already Exists"
-         exit
+         
 else
         mkdir $path/$dir1
         echo "$dir1 Created Successfully so good exit"
-	exit
 fi
 
